5473be35dae5475106d14429a43f9cb507417b85
Ram Sai Karthik Unnam <ramsai.karthik@gmail.com>
2021-08-03 23:41:38 +0530

    reads avro, converts to csv, adds a timestamp column

--- Structured_Operations_Pyspark/venv/ReadAvro.py
+++ Structured_Operations_Pyspark/venv/ReadAvro.py
@@ -8,5 +8,19 @@ spark = SparkSession.builder.appName("Structured Data").config("spark.driver.mem
 spark.sparkContext.addPyFile("C:\\Softwares\\Spark\\pyspark\\jars\\org.apache.spark_spark-avro_2.12-3.1.2.jar")
 
 dataFrame = spark.read.format("avro").load("C:\\Users\\Ram Sai Karthik\\Desktop\\userdata1.avro")
+print("Total number of rows in dataFrame are: ",dataFrame.count())
+dataFrame.printSchema()
+print("AVRO dataframe is: ")
+dataFrame.show(5,False)
 
-dataFrame.show()
+#Coalesce is to have the number of output files
+dataFrame.limit(50).coalesce(1).write.mode("overwrite").csv("D:\\PythonPrograms\\Structured_Operations_Pyspark\\OutputFiles\\AvroFile1", header=True)
+csvDataFrame = spark.read.csv("D:\\PythonPrograms\\Structured_Operations_Pyspark\\OutputFiles\\AvroFile1\\*.csv", inferSchema=True, header=True)
+print("Total record count in csv file is: ",csvDataFrame.count())
+
+#Added a column for timeStamp
+
+csvDataFrameTimeStamp = csvDataFrame.withColumn("processed_date_time", to_utc_timestamp(current_timestamp(),"IST"))
+csvDataFrameTimeStamp.printSchema()
+print("CSV dataframe is: ")
+csvDataFrameTimeStamp.show(5,False)
