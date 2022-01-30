c05355899c294f3506e4517c1903b2fdb4b3c9a1
Ram Sai Karthik Unnam <ramsai.karthik@gmail.com>
2022-01-30 22:50:42 +0530

    List and Tuple

--- Python General Programming/Python/Lists_Tuples.py
+++ Python General Programming/Python/Lists_Tuples.py
@@ -1,4 +1,5 @@
 list1 = ["Ram","Sai","Karthik","Unnam"]
+list2 = ["Daddy","Mom","Chitti"]
 print(list1)
 tuple1 = ("Ram","Sai","Karthik","Unnam")
 print(tuple1)
@@ -6,4 +7,14 @@ print(list1[2])
 print(tuple1[2])
 print(list1.insert(4,"NewElement"))
 print(list1)
-print(tuple1.index("Ram"))
+print("Ram is located at index: ",tuple1.index("Ram"))
+list1.append("Ram")
+print(list1)
+list1.sort()
+print(list1)
+list1.append(list2)
+print("Each and every element in the list is: ")
+for i in range(len(list1)):
+    print(list1[i])
+
+print(list1[len(list1)-1][1])
