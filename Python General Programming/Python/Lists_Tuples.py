2ab1556c52292d38c4a874dc0c4a8752662b1a0b
Ram Sai Karthik Unnam <ramsai.karthik@gmail.com>
2022-01-30 21:51:23 +0530

    List and Tuple

---
+++ Python General Programming/Python/Lists_Tuples.py
@@ -0,0 +1,9 @@
+list1 = ["Ram","Sai","Karthik","Unnam"]
+print(list1)
+tuple1 = ("Ram","Sai","Karthik","Unnam")
+print(tuple1)
+print(list1[2])
+print(tuple1[2])
+print(list1.insert(4,"NewElement"))
+print(list1)
+print(tuple1.index("Ram"))
