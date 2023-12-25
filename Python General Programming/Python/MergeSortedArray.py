c390c7365109608af059054319e1de67b0621789
Ram Sai Karthik Unnam <ramsai.karthik@gmail.com>
2023-12-25 17:52:30 +0530

    MergeSortedArray

--- Python General Programming/Python/MergeSortedArray.py
+++ Python General Programming/Python/MergeSortedArray.py
@@ -1,5 +1,5 @@
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
@@ -7,10 +7,30 @@ class Solution:
             nums1 = nums2[:n].sort()
         if n==0 or len(nums2)==0:
             nums1 = nums1[:m].sort()
-        else:
+        if m>0 and n>0:
             nums1 = nums1[0:m]
             nums2 = nums2[0:n]
             nums1+=nums2
             nums1.sort()
         print(nums1)
-        
+
+mer = Solution()
+# nums1 = [1,2,3,0,0,0]
+# m = 3
+# nums2 = [2,5,6]
+# n = 3
+
+# nums1 = [1]
+# m = 1
+# nums2 = []
+# n = 0
+# nums3=nums1+nums2
+
+nums1 = []
+m = 0
+nums2 = [1]
+n = 1
+print(nums1[0:m]+nums2[0:n])
+print(len(nums1))
+print(len(nums2))
+mer.merge(nums1, m, nums2, n)
